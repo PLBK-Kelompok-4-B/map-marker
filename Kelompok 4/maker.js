3f05a7be5e6447f5e022448bdd25987077c30ed0
Mulia Andiki <muliaandiki@gmail.com>
2026-04-26 08:56:57 +0700

    feat: initial map for application

---
+++ Kelompok 4/maker.js
@@ -0,0 +1,15 @@
+const markersDiki = [
+  {
+    nama: "Kost Diki",
+    lat: 5.571722,
+    lng: 95.355611,
+  },
+];
+
+const markersDayat = [
+  // Isi marker Mahardika di sini
+];
+
+const markersMirdas = [
+  // Isi marker Irfan di sini
+];
